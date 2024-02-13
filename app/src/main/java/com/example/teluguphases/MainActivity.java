8c606329666dcdc041bb87111a73d0755a7248f3
Navdeep Kumar <10navdeepkumar@gmail.com>
2024-02-13 12:58:14 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/teluguphases/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.teluguphases;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void playPhase(View view){
+        Button buttonPressed = (Button) view;
+        Log.i("Button Pressed", buttonPressed.getTag().toString());
+        MediaPlayer mPlayer = MediaPlayer.create(this, getResources().getIdentifier(buttonPressed.getTag().toString(), "raw", getPackageName()));
+        mPlayer.start();
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
